fcd54bacb36048d21b7332e7a37c511f79b2f874
Wilmer H <wilmer.hernandez@gmail.com>
2021-05-01 08:11:27 -0500

    Se agregó el   intl: ^0.17.0, pero siguen existiendo errores en el código anterior.

--- lib/widget/card.widget.dart
+++ lib/widget/card.widget.dart
@@ -9,11 +9,18 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(articulo.title),
-      decoration: BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.circular(10.0),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        children: [
+          Text(articulo.publishedAt.toString()),
+          Text(articulo.title),
+          Text(articulo.description),
+        ],
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }

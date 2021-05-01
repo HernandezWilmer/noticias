28020644e7cd5d86f22d28c51ad14160d3a72676
Wilmer H <wilmer.hernandez@gmail.com>
2021-05-01 08:28:37 -0500

    Se agregó código para cargar el GIF que suministró el ingeniero.

--- lib/widget/card.widget.dart
+++ lib/widget/card.widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:noticias/models/articulos.model.dart';
 
 class CardWidget extends StatelessWidget {
@@ -12,11 +13,29 @@ class CardWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        children: [
-          Text(articulo.publishedAt.toString()),
-          Text(articulo.title),
-          Text(articulo.description),
-        ],
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text(DateFormat('yyy:MM:DD').format(articulo.publishedAt)),
+                ],
+              ),
+            ),
+            FadeInImage(
+                placeholder: AssetImage('assets/loading.gif'),
+                image: NetworkImage(articulo.urlToImage)),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(articulo.title),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(articulo.description),
+            ),
+          ],
+        ),
         decoration: BoxDecoration(
           color: Colors.red,
           borderRadius: BorderRadius.circular(10.0),

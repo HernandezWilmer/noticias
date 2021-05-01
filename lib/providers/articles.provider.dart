07f8db9b99904b9b7d73fe1835f0733d2f5db4f0
Wilmer H <wilmer.hernandez@gmail.com>
2021-04-30 19:38:26 -0500

    Comenzamos.

---
+++ lib/providers/articles.provider.dart
@@ -0,0 +1,25 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+class ArticleProvider {
+  getArticles() async {
+    Map<String, String> queryParameters = {
+      'q': 'tesla',
+      'from': '2021-03-30',
+      'sortBy': 'publishedAt',
+      'apiKey': 'cb138a72ec374a15abe19c412d8da9d1',
+    };
+
+    var url = Uri.https('newsapi.org', '/v2/everything', queryParameters);
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var itemCount = jsonResponse['totalItems'];
+      print('Number of books about http: $itemCount.');
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+}
